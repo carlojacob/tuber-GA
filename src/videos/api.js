d6ce183ae3a5a90fe4b0fdcf0a399d35a5db5dcc
carlojacob <carlo.jacob@gmail.com>
2019-03-11 14:04:34 -0400

    Add Videos component
    - Add `AuthenticatedRoute` to Videos in `App.js`.
    - Add `Link` to `/videos` to `authenticatedOptions` in `Header.js`.
    - Copy `api.js` from auth, add `getVideos` `axios` request.
    - Copy `messages.js` from auth, create responses for index request.
    - Generate `Videos` component which indexes user videos.
    - Add minimal SCSS to display videos.

---
+++ src/videos/api.js
@@ -0,0 +1,49 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const signUp = credentials => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/sign-up',
+    data: {
+      credentials: {
+        email: credentials.email,
+        password: credentials.password,
+        password_confirmation: credentials.passwordConfirmation
+      }
+    }
+  })
+}
+
+export const signIn = credentials => {
+  return axios({
+    url: apiUrl + '/sign-in',
+    method: 'POST',
+    data: {
+      credentials: {
+        email: credentials.email,
+        password: credentials.password
+      }
+    }
+  })
+}
+
+export const signOut = user => {
+  return axios({
+    url: apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+
+export const getVideos = (user) => {
+  return axios({
+    url: apiUrl + '/videos',
+    method: 'GET',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
