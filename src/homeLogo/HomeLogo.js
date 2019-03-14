9b32b271d118b710453e861cb4fb85355a3d76f6
carlojacob <carlo.jacob@gmail.com>
2019-03-13 20:07:27 -0400

    Add Logos to Home Page and nav bar
    - Store small and large logos files for reference.
    - Add small logo to nav bar in `Header.js`.
    - Add styling to adjust size of small logo in `Header.scss`.
    - Create `HomeLogo` component to display large logo on homepage.
    - Add styles to locate and adjust size of large logo in `HomeLogo.scss`.
    - Add `Route` to `HomeLogo` in `App.js`.

---
+++ src/homeLogo/HomeLogo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './HomeLogo.scss'
+
+const HomeLogo = () => (
+  <div className="home-logo-div">
+    <img className="home-logo" src="https://i.imgur.com/by4qHWe.png" title="tuberLargeLogo" />
+  </div>
+)
+
+export default HomeLogo
