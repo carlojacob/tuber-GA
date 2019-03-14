2de6d39c9fe78d81ecb7c6b6d56d0715130f6b7c
carlojacob <carlo.jacob@gmail.com>
2019-03-13 21:34:37 -0400

    Use Local Image Logo Files
    - Move `tuberLargeLogo` to `homeLogo` folder.
    - Require logo images for use in `Header.js`, `HomeLogo.js`.

--- src/homeLogo/HomeLogo.js
+++ src/homeLogo/HomeLogo.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import './HomeLogo.scss'
 
+const tuberLargeLogo = require('./tuberLargeLogo')
+
 const HomeLogo = () => (
   <div className="home-logo-div">
-    <img className="home-logo" src="https://i.imgur.com/by4qHWe.png" title="tuberLargeLogo" />
+    <img className="home-logo" src={tuberLargeLogo} title="tuberLargeLogo" />
   </div>
 )
 
